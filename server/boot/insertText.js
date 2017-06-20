92939bcd024d26cf4bd33f34ea1dfd4b30864bb0
Peter Horton <phorton@softstackfactory.org>
2017-06-20 04:57:38 +0000

    First commit

---
+++ server/boot/insertText.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+
+	var jsonArr = require('../questions.json');
+
+	var Question = app.models.Questions;
+
+	Question.destroyAll();
+	
+
+	Question.upsert(jsonArr, function(err, record) {
+			if (err) return console.log(err);
+		});
+
+	console.log("Questions inserted successfully");
+};
